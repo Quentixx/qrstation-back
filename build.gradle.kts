3cbe36d63ee786e07f12d5ba6d82db1f50fdbaa0
Quentin <nelsonsims@hotmail.fr>
2023-07-22 19:13:31 +0200

    deps: Init the project

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("org.springframework.boot") version("2.7.8")
+    java
+}
+
+group = "fr.quentixx"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_16
+    targetCompatibility = JavaVersion.VERSION_16
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("org.springframework.boot:spring-boot-dependencies:2.7.8"))
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    runtimeOnly("com.h2database:h2")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(mapOf("group" to "org.junit.vintage", "module" to "junit-vintage-engine"))
+    }
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
