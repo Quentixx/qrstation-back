0f76a20b519f7f0cf695a74f54da07d47b6ce574
Quentin <nelsonsims@hotmail.fr>
2023-07-22 19:14:03 +0200

    deps: Created the rest controller

---
+++ src/main/java/qrstation/back/rest/QRCodeController.java
@@ -0,0 +1,46 @@
+package qrstation.back.rest;
+
+import qrstation.back.data.QRCode;
+import qrstation.back.data.QRCodeRequest;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/qrcodes")
+public class QRCodeController {
+
+    private final Map<String, QRCode> qrcodes = new HashMap<>();
+
+    @PostMapping
+    public QRCode createQRCode(@RequestBody QRCodeRequest request) {
+        String uuid = UUID.randomUUID().toString();
+        QRCode qrCode = new QRCode(uuid, request.redirectUrl());
+        qrcodes.put(uuid, qrCode);
+        return qrCode;
+    }
+
+    @GetMapping
+    public List<QRCode> getAllQRCodes() {
+        return new ArrayList<>(qrcodes.values());
+    }
+
+    @GetMapping("/{uuid}")
+    public QRCode getQRCode(@PathVariable String uuid) {
+        return qrcodes.get(uuid);
+    }
+
+    @PutMapping("/{uuid}")
+    public QRCode updateQRCode(@PathVariable String uuid, @RequestBody QRCodeRequest request) {
+        QRCode qrCode = qrcodes.get(uuid);
+        if (qrCode != null) {
+            qrCode.setRedirectUrl(request.redirectUrl());
+        }
+        return qrCode;
+    }
+
+    @DeleteMapping("/{uuid}")
+    public boolean deleteQRCode(@PathVariable String uuid) {
+        return qrcodes.remove(uuid) != null;
+    }
+}
