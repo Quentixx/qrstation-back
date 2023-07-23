984479a5d68285935bea7e28bb927da140418c0f
Quentin <nelsonsims@hotmail.fr>
2023-07-23 17:28:06 +0200

    deps: Added database annotations in data class

--- src/main/java/qrstation/back/database/data/QRCode.java
+++ src/main/java/qrstation/back/database/data/QRCode.java
@@ -1,11 +1,18 @@
-package qrstation.back.data;
+package qrstation.back.database.data;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
 
 /**
  * Represents a QR Code by a UUID associated with an url.
  */
+@DatabaseTable(tableName = "qrcodes")
 public class QRCode {
 
+    @DatabaseField(id = true)
     private final String uuid;
+
+    @DatabaseField
     private String redirectURL;
 
     public QRCode(String uuid, String redirectURL) {
